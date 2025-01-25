f3df6044ec80662ca3a4aafd91c8c5071e67beb7
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2025-01-25 14:16:34 -0300

    feat: Radix React Icons Library feat: Sidebar component
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 import { Header } from './components/header';
+import { Sidebar } from './components/sidebar';
 
 export function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+
+      <main className="max-w-[70rem] my-8 mx-auto py-0 px-4 grid grid-cols-[256px_1fr] gap-8 items-start">
+        <Sidebar />
+
+        <section>Main Post</section>
+      </main>
+    </>
+  );
 }
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,40 @@
+import { Pencil2Icon } from '@radix-ui/react-icons';
+
+export function Sidebar() {
+  return (
+    <aside className="bg-woodsmoke-800 rounded-lg overflow-hidden">
+      <img
+        src="https://images.unsplash.com/photo-1593079831268-3381b0db4a77?q=80&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Background Gym Cover Img"
+        className="w-full h-[4.5rem] object-cover"
+      />
+
+      <div className="flex flex-col items-center -mt-6">
+        <img
+          src="https://github.com/LucasAnselmoSilva12345.png"
+          alt="Github Image profile Lucas Anselmo"
+          className="bg-woodsmoke-800 p-1 w-14 h-14 rounded-lg border-2 border-eucalyptus-600 "
+        />
+
+        <div className="pt-4 flex flex-col items-center justify-center gap-1">
+          <strong className="text-base font-roboto-bold text-woodsmoke-200">
+            Lucas Anselmo
+          </strong>
+          <span className="text-sm text-woodsmoke-400">
+            Front-end Software Developer
+          </span>
+        </div>
+      </div>
+
+      <footer className="border-t border-solid border-t-neutral-700 mt-6 pt-6 px-8 pb-8">
+        <a
+          href="#"
+          className="py-4 px-6 flex items-center justify-center gap-2 bg-transparent text-sm font-bold font-roboto-bold text-eucalyptus-500 border border-solid border-eucalyptus-500 rounded-lg transition-colors hover:bg-eucalyptus-700 hover:text-eucalyptus-50 hover:border-emerald-700"
+        >
+          <Pencil2Icon className="w-4 h-4" />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
