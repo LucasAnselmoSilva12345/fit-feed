f754843ab138f1e8af06d1807b2a9ba0d3066a36
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2025-02-16 13:36:08 -0300

    feat: Fit-Feed bot
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/comment.tsx
+++ src/components/comment.tsx
@@ -22,21 +22,21 @@ export function Comment({ content, onDeleteComment }: IComment) {
 
   return (
     <div className="flex items-start gap-2 lg:gap-4">
-      <Avatar githubURL="https://github.com/LucasAnselmoSilva12345.png" />
+      <Avatar githubURL="https://live.staticflickr.com/65535/54331543968_35955063b7_o.png" />
 
       <div className="w-full">
         <div className="bg-woodsmoke-900 rounded-lg px-4 pt-4 pb-6 space-y-4">
           <header className="flex items-start">
             <div>
               <h3 className="text-sm font-roboto-bold font-bold text-woodsmoke-200">
-                Devon Lane <span className="text-woodsmoke-400">(você)</span>
+                FitGuru <span className="text-woodsmoke-400">(bot)</span>
               </h3>
               <time
                 title="25 de Janeiro às 14:47"
                 dateTime="2025-01-25 14:48:30"
                 className="ml-auto text-xs text-woodsmoke-400"
               >
-                Cerca de 1h atrás
+                Agora mesmo
               </time>
             </div>
             <button
